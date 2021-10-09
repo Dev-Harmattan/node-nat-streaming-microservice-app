3db7c37392ad3c1e4339489508c12c1e435e302d
Tunde Abolaji <tundeabolaji@Tundes-MacBook-Pro.local>
2021-10-08 23:56:37 -0600

    Ingress config file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 
 
+app.get('/api/user/currentUser', (req, res) => {
+  res.send('Current User');
+})
+
 const PORT = 3000;
 app.listen(3000, () => {
   console.log(`Auth running on port: ${PORT}`);
