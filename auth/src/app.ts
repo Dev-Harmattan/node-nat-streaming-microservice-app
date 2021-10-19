8dac9c5d7fcb50085dacb79d56f824023ece76ab
Tunde Abolaji <tundeabolaji@Tundes-MacBook-Pro.local>
2021-10-19 09:45:35 -0600

    jest and supertest library  setup for test

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cookieSession from 'cookie-session';
+import 'express-async-errors';
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+import {signupRouter} from './routes/signup';
+import {errorMiddleware} from './middleware/errorMiddleware';
+import {NotFoundError} from './errors/NotFoundError';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}))
+
+//router middleware
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+})
+
+app.use(errorMiddleware);
+
+export {app}
